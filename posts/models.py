0905ed95b421341c2340e97057c148cda42ed61a
Teresa Dinis <7661385@formacao.iefp.pt>
2024-10-21 12:51:50 +0100

    app posts com categorias

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    titulo = models.CharField('título', max_length=200)
-    image = models.ImageField(upload_to="images/")
-    data = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
 
     def __str__(self) -> str:
-        return self.titulo + " - " + self.image.url
+        return self.nome
+
+
+class Post(models.Model):
+    titulo = models.CharField('título', max_length=200)
+    imagem = models.ImageField(upload_to="images/")
+    data = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, default=1)
+
+    def __str__(self) -> str:
+        return self.titulo + " - " + self.imagem.url
         #return self.titulo
